bba5211495ee06aa1d0d0589e0e2f856777dd948
arqdsis <arqdsis@MOOLIi0108.animaedu.intranet>
2020-02-18 22:22:44 -0300

    Hello,

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld /*como a classe é publica, o nome da classe está restrito ao nome do arquivo*/
+{
+    public static void main (String [] args)
+    {
+        System.out.println ("Hello, ");
+    } 
+} 
